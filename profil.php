ab2c1827dcbfac13011255825d1972de8dcda162
yonathan-darmon <yonathan.darmon@laplateforme.io>
2021-11-16 16:36:37 +0100

    fin de l archi de profil (req d update)

--- profil.php
+++ profil.php
@@ -1,13 +1,24 @@
 <?php
 session_start();
-echo '<h1>Bienvenue ' . $_SESSION['login'] .'</h1>';
-$login=$_SESSION['login'];
-$mdp=$_SESSION['password'];
+if (!isset($_SESSION['login'])) {
+    header("location: connexion.php");
+}
+echo '<h1>Bienvenue ' . $_SESSION['login'] . '</h1>';
+$login = $_SESSION['login'];
+$mdp = $_SESSION['password'];
+
 $connect = mysqli_connect("localhost", "root", "", "moduleconnexion"); /*connexion a la base*/
-$req= mysqli_query($connect,"SELECT * FROM utilisateurs WHERE password='$mdp' AND login='$login' ") ;
-$table=mysqli_fetch_all($req,MYSQLI_ASSOC);
-var_dump($table);
-foreach ($table as $key=>$value);
+$req = mysqli_query($connect, "SELECT * FROM utilisateurs WHERE password='$mdp' AND login='$login' ");
+$table = mysqli_fetch_all($req, MYSQLI_ASSOC);
+foreach ($table as $key => $value) ;
+if (isset($_POST['submit']) && $_POST['password'] === $_POST['confirm']) {
+    $util = $_POST['login'];
+    $prenom = $_POST['prenom'];
+    $nom = $_POST['nom'];
+    $password = $_POST['password'];
+    $update = mysqli_query($connect, "UPDATE utilisateurs SET login = '$util', password = '$mdp', prenom = '$prenom',nom = '$nom',password = '$password' WHERE login = '$login' AND password = '$mdp'");
+    mysqli_query($connect, $update);
+}
 ?>
 <!doctype html>
 <html lang="en">
@@ -23,15 +34,15 @@ foreach ($table as $key=>$value);
 <main>
     <form action="#" method="post">
         <label for="login">Nom d'utilisateur</label>
-        <input type="text" name="login" value=<?php echo $value['login'];?> >
+        <input type="text" name="login" value=<?php echo $value['login']; ?>>
         <label for="prenom">Prénom</label>
-        <input type="text" name="prenom" value=<?php echo $value['prenom'];?> >
+        <input type="text" name="prenom" value=<?php echo $value['prenom']; ?>>
         <label for="nom">Nom</label>
-        <input type="text" name="nom" value=<?php echo $value['nom'];?> >
+        <input type="text" name="nom" value=<?php echo $value['nom']; ?>>
         <label for="password">Mot de passe</label>
-        <input type="text" name="password" value=<?php echo $value['password'];?> >
+        <input type="text" name="password" value=<?php echo $value['password']; ?>>
         <label for="confir">Confirmer votre mot de passe</label>
-        <input type="text" name="confirm" value=<?php echo $value['password'];?>>
+        <input type="text" name="confirm" value=<?php echo $value['password']; ?>>
         <input type="submit" value="envoyer" name="submit">
     </form>
 </main>
